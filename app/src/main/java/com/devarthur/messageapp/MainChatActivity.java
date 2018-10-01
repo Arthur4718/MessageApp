2278778bb04f01ee2ba45b8096190d164f954d16
Unknown <arthur.gomes_4718@hotmail.com>
2018-09-30 21:27:45 -0300

    Instant message Class.
    Added some new come to MainActivity in order to write and read data from firebase. Also added the listener to fire the sendMessage method once the user typed a message. Added the InstantMessage Class to handle message creatings and its use with Firebase.

--- app/src/main/java/com/devarthur/messageapp/MainChatActivity.java
+++ app/src/main/java/com/devarthur/messageapp/MainChatActivity.java
@@ -1,26 +1,39 @@
 package com.devarthur.messageapp;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.util.Log;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 
 public class MainChatActivity extends AppCompatActivity {
 
-    // TODO: Add member variables here:
+
     private String mDisplayName;
     private ListView mChatListView;
     private EditText mInputText;
     private ImageButton mSendButton;
+    private DatabaseReference mDatabaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_chat);
 
-        // TODO: Set up the display name and get the Firebase reference
+
+        setupDisplayName();
+        //Call the reference used to write and read data on Firebase.
+        mDatabaseReference = FirebaseDatabase.getInstance() .getReference();
+
 
 
         // Link the Views in the layout to the Java code
@@ -28,18 +41,38 @@ public class MainChatActivity extends AppCompatActivity {
         mSendButton = (ImageButton) findViewById(R.id.sendButton);
         mChatListView = (ListView) findViewById(R.id.chat_list_view);
 
-        // TODO: Send the message when the "enter" button is pressed
 
+        mInputText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                sendMessage();
+                return false;
+            }
+        });
 
-        // TODO: Add an OnClickListener to the sendButton to send a message
+        mSendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendMessage();
+            }
+        });
 
     }
 
-    // TODO: Retrieve the display name from the Shared Preferences
+    private void setupDisplayName(){
+        SharedPreferences prefs = getSharedPreferences(RegisterActivity.CHAT_PREFS, MODE_PRIVATE);
+
+        mDisplayName = prefs.getString(RegisterActivity.DISPLAY_NAME_KEY, null);
+
+        if(mDisplayName == null) mDisplayName = "Anonymous";
+    }
+
+
 
 
     private void sendMessage() {
 
+        Log.d("App", "Message Sent");
         // TODO: Grab the text the user typed in and push the message to Firebase
 
     }
