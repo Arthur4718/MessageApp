60bd2b453c7b88301acebf1af165d460c90a51f9
Unknown <arthur.gomes_4718@hotmail.com>
2018-10-02 23:41:21 -0300

    Chat Row Appearance.
    Added a new method into the ChatListAdapter that checks for the messages's author. If it is your message, then we set a green colour, if its not, then we set a blue colour.

--- app/src/main/java/com/devarthur/messageapp/ChatListAdapter.java
+++ app/src/main/java/com/devarthur/messageapp/ChatListAdapter.java
@@ -2,8 +2,10 @@ package com.devarthur.messageapp;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Color;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -116,6 +118,10 @@ public class ChatListAdapter extends BaseAdapter{
         final InstantMessage message = getItem(position);
         final ViewHolder holder = (ViewHolder) convertView.getTag();
 
+        //Get hold of the user and check if the messages in the display are his messages.
+        //If the messages are from this user, then we are going to change the body background.
+        boolean isMe = message.getAuthor().equals(mDisplayName);
+        setChatRowAppearance(isMe,holder);
 
         String author = message.getAuthor();
         holder.authorName.setText(author);
@@ -125,6 +131,23 @@ public class ChatListAdapter extends BaseAdapter{
 
 
         return convertView;
+    }
+
+    private void setChatRowAppearance(boolean isItMe, ViewHolder holder){
+
+        if(isItMe){
+            holder.params.gravity = Gravity.END;
+            holder.authorName.setTextColor(Color.GREEN);
+            holder.body.setBackgroundResource(R.drawable.bubble2);
+        }else{
+            holder.params.gravity = Gravity.START;
+            holder.authorName.setTextColor(Color.BLUE);
+            holder.body.setBackgroundResource(R.drawable.bubble1);
+        }
+
+        holder.authorName.setLayoutParams(holder.params);
+        holder.body.setLayoutParams(holder.params);
+
     }
 
     public void cleanup(){
